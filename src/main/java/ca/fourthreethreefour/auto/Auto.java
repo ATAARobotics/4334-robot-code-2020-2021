12ac588e5a4d2f40b7837c10a72bf6cc05127ec0
MatthewEngelman <matthewengelman18@gmail.com>
2020-01-27 18:44:28 -0700

    Set up AutoFile in Auto

--- src/main/java/ca/fourthreethreefour/auto/Auto.java
+++ src/main/java/ca/fourthreethreefour/auto/Auto.java
@@ -1,20 +1,32 @@
 package ca.fourthreethreefour.auto;
 
+import java.io.File;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class Auto {
+   private AutoFile autoFile = null;
+
     public Auto() {
 
     }
 
     public void autoInit() {
-
+        try {
+            autoFile = new AutoFile(new File("/auto.txt"));
+        } catch (Exception e) {
+            DriverStation.reportWarning("no auto file detected!", true);
+        }
+        autoFile.init();
+        
     }
 
     public void autoPeriodic() {
-
+        autoFile.run();
     }
 
     public void autoDisabled() {
-
+        autoFile.end();
     }
 
 }
