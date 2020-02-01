f140d6d464fbe6ae3f566acea8804df7eb3f5f05
Gio <canada.gio@gmail.com>
2020-01-31 19:08:48 -0700

    Made the starting to the PID drive subsystem

---
+++ src/main/java/ca/fourthreethreefour/subsystems/pid/DrivePID.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package ca.fourthreethreefour.subsystems.pid;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpiutil.math.MathUtil;
+
+public class DrivePID extends PIDSubsystem {
+  /**
+   * Creates a new DrivePID.
+   */
+  private double speed;
+  public DrivePID() {
+    super(
+        // The PIDController used by the subsystem
+        new PIDController(0, 0, 0));
+  }
+
+  @Override
+  public void useOutput(double output, double setpoint) {
+    // Use the output here
+    speed = MathUtil.clamp(output, -1, 1);
+  }
+
+  @Override
+  public double getMeasurement() {
+    // Return the process variable measurement here
+    return 0;
+  }
+
+  /**
+   * 
+   * @return Speed value
+   */
+  public double getSpeed() {
+    return speed;
+  }
+}
