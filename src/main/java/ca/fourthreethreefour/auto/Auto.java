5d0632e356f94e6731f1e1e7535dee4551651560
Kulkinz <cool@coolspages.com>
2020-02-26 12:21:40 -0700

    auto dropdown box

--- src/main/java/ca/fourthreethreefour/auto/Auto.java
+++ src/main/java/ca/fourthreethreefour/auto/Auto.java
@@ -9,20 +9,32 @@ import ca.fourthreethreefour.subsystems.Shooter;
 import ca.fourthreethreefour.subsystems.pid.AlignPID;
 import ca.fourthreethreefour.subsystems.pid.DrivePID;
 import ca.fourthreethreefour.subsystems.pid.FlywheelPID;
+import ca.fourthreethreefour.subsystems.pid.HoodPID;
 import ca.fourthreethreefour.subsystems.pid.TurnPID;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 public class Auto {
     private AutoFile autoFile = null;
+    private SendableChooser<File> selectedAuto = new SendableChooser<>();
 
-    public Auto(Drive driveSubsystems, Shooter shooterSubsystem, Cartridge cartridgeSubsystem, Intake intakeSubsystem, DrivePID drivePID, TurnPID turnPID, FlywheelPID flywheelPID, AlignPID alignPID) {
+    public Auto(Drive driveSubsystems, Shooter shooterSubsystem, Cartridge cartridgeSubsystem, Intake intakeSubsystem, DrivePID drivePID, TurnPID turnPID, FlywheelPID flywheelPID, AlignPID alignPID, HoodPID hoodPID) {
         autoFile = new AutoFile(driveSubsystems, shooterSubsystem, cartridgeSubsystem,
-                intakeSubsystem, drivePID, turnPID, flywheelPID, alignPID);
+                intakeSubsystem, drivePID, turnPID, flywheelPID, alignPID, hoodPID);
+        ShuffleboardTab autoTab = Shuffleboard.getTab("Auto");
+        selectedAuto.setDefaultOption("Move off line", new File("/FILES/auto_moveoffline.txt"));
+        selectedAuto.addOption("Test", new File("/FILES/auto_test.txt"));
+        autoTab.add("Auto Selector", selectedAuto).withWidget(BuiltInWidgets.kComboBoxChooser);
     }
 
     public void autoInit() {
         try {
-            autoFile.init(new File("/auto.txt"));
+            autoFile.init(selectedAuto.getSelected());
         } catch (Exception e) {
             DriverStation.reportWarning("no auto file detected!", false);
         }
